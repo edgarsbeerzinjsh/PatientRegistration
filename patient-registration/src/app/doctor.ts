926c92d6a6a701587477e351e47c4b6ccc3c389d
edgars.beerzinjsh <103140210+edgarsbeerzinjsh@users.noreply.github.com>
2023-06-29 11:00:36 +0300

    Angular part created

---
+++ patient-registration/src/app/doctor.ts
@@ -0,0 +1,9 @@
+export interface Doctor {
+    id?: number;
+    name: string;
+    surname: string;
+    telephone: string;
+    email: string;
+    birthDate: Date;
+    doctorsId: number[];
+}
---
+++ patient-registration/src/app/patient.ts
@@ -0,0 +1,9 @@
+export interface Patient {
+    id?: number;
+    name: string;
+    surname: string;
+    telephone: string;
+    email: string;
+    birthDate: Date;
+    doctorsId: number[];
+}
